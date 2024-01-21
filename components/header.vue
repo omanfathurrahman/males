1024490b596190f64923ddc12aff0b343f70daa5
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-22 02:40:23 +0700

    Add NuxtLayout and NuxtPage components, and include @nuxtjs/tailwindcss module

---
+++ components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between items-center gap-24 px-[6rem] border h-[4rem]">
+    <img src="/logo.svg" alt="">
+    <div class="flex gap-8">
+      <a href="#">Home</a>
+      <a href="#">Mapel</a>
+      <a href="#">Latihan Soal</a>
+      <a href="#">Tentang Kami</a>
+    </div>
+  </div>
+</template>
