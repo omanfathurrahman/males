14f312e0c40e09955e6a40bc80bbf49f6b3dc9c4
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-24 00:36:47 +0700

    Add Mata Pelajaran page with content

---
+++ pages/mata-pelajaran/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col py-[1rem] gap-6 px-[1rem] sm:px-[8rem] mt-[4rem]" >
+    <h2 class="text-2xl">Daftar Mata Pelajaran</h2>
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-5 gap-y-3">
+      <div class="border shadow-m rounded-2xl px-5 py-3">
+        <Icon name="tabler:pig-money" class="text-4xl mb-1 text-green-500" />
+        <h4 class="text-lg font-medium">Akses Gratis ke Konten Belajar</h4>
+        <p>Kami bangga menyediakan akses gratis ke semua konten belajar di platform kami. </p>
+      </div>
+      <div class="border shadow-m rounded-2xl px-5 py-3">
+        <Icon name="material-symbols:alarm-on-outline-rounded" class="text-4xl mb-1 text-yellow-500" />
+        <h4 class="text-lg font-medium">Tidak Ada Batasan Waktu</h4>
+        <p>Di Beabai, tidak ada batasan waktu dalam mengakses materi belajar.</p>
+      </div>
+      <div class="border shadow-m rounded-2xl px-5 py-3">
+        <Icon name="material-symbols:ballot-outline-rounded" class="text-4xl mb-1 text-blue-500" />
+        <h4 class="text-lg font-medium">Beragam Mata Pelajaran</h4>
+        <p>Kami menawarkan beragam mata pelajaran yang meliputi matematika, bahasa Inggris, sains, sejarah, dan banyak lagi.</p>
+      </div>
+      <div class="border shadow-m rounded-2xl px-5 py-3">
+        <Icon name="ic:twotone-live-help" class="text-4xl mb-1 text-violet-500" />
+        <h4 class="text-lg font-medium">Latihan Soal Interaktif</h4>
+        <p>Kami menyediakan latihan soal interaktif yang membantu Anda menguji pemahaman dan keterampilan Anda.</p>
+      </div>
+    </div>
+  </div>
+</template>
